645d83a5a69a3105a23af33efa0edb535326c31e
tosm01 <taha2013.to@gmail.com>
2022-11-22 15:38:32 +0800

    Project initialisation

---
+++ cryptocurrencies/src/main/java/com/cryptotracker/cryptocurrencies/CryptocurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.cryptotracker.cryptocurrencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrenciesApplication.class, args);
+	}
+
+}
---
+++ cryptocurrencies/src/test/java/com/cryptotracker/cryptocurrencies/CryptocurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptotracker.cryptocurrencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
